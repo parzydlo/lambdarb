75ed523e3f09a85c8385db15a4850a908dd15c35
Jan Parzydło <janparzydlo@gmail.com>
2019-05-25 16:29:27 +0100

    add visitor pattern

--- ast.rb
+++ ast.rb
@@ -1,1 +1,46 @@
-ASTNode = Struct.new(:ast_type, :ast_children)
+class ASTNode
+    attr_reader :children, :pos
+
+    def initialize(children, pos)
+        @children = children
+        @pos = pos
+    end
+
+    def accept(visitor)
+        visitor.visit(self)
+        @children.each { |child| child.accept(visitor) } unless @children.nil?
+    end
+end
+
+class ExprNode < ASTNode
+    def initialize(children, pos)
+        super(children, pos)
+    end
+end
+
+class AbstrNode < ASTNode
+    def initialize(children, pos)
+        super(children, pos)
+    end
+end
+
+class AppNode < ASTNode
+    def initialize(children, pos)
+        super(children, pos)
+    end
+end
+
+class IdNode < ASTNode
+    attr_reader :value
+
+    def initialize(children, pos, value)
+        super(children, pos)
+        @value = value
+    end
+end
+
+class AtomNode < ASTNode
+    def initialize(children, pos)
+        super(children, pos)
+    end
+end
--- lambda.rb
+++ lambda.rb
@@ -1,33 +1,20 @@
 require_relative 'lexer.rb'
 require_relative 'parser.rb'
-require_relative 'utils.rb'
-# Original grammar:
-# TERM        -> APPLICATION | ABSTRACTION
-# ABSRTACTION -> LAMBDA LCID DOT TERM
-# APPLICATION -> APPLICATION ATOM | ATOM
-# ATOM        -> LPAREN TERM RPAREN | LCID
-#
-# without left-recursion:
-# TERM         -> APPLICATION | ABSTRACTION
-# ABSRTACTION  -> LAMBDA LCID DOT TERM
+require_relative 'print_visitor.rb'
+
+# Grammar:
+# EXPR         -> APPLICATION | ABSTRACTION
+# ABSTRACTION  -> LAMBDA LCID DOT EXPR
 # APPLICATION  -> ATOM APPLICATION'
 # APPLICATION' -> ATOM APPLICATION' | ε       
-# ATOM         -> LPAREN TERM RPAREN | LCID
-# LAMBDA       -> λ | \
+# ATOM         -> LPAREN EXPR RPAREN | LCID
+# LAMBDA       -> λ | /
 # DOT          -> .
 # LCID         -> a | b | ... | z
-#
-# FIRST sets:
-# LCID : { w : w ~= /a-z/ }
-# DOT : { . }
-# LAMBDA : { λ, \ }
-# ATOM : { ( } v FIRST(LCID)
-# APPLICATION : {  }
 
 l = Lexer.to_proc
 p = Parser.to_proc
-#e = Evaluator.to_proc
 
-input = ARGV[0]
+input = "(λx.x)(λy.y)"
 ast = p[l[input]]
-pretty_print_ast(ast)
+ast.accept(PrintVisitor.new)
